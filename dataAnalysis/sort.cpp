b548e6638dcb80c5d46a5f3c940c5f4138325be6
pearmeow <perryhuang2015@gmail.com>
2024-05-05 21:11:02 -0400

    idk

--- dataAnalysis/sort.cpp
+++ dataAnalysis/sort.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 
 int main() {
-  std::vector<double> v{18.1, 20.3, 18.3, 15.6, 22.5, 16.8, 17.6, 16.9, 18.2, 17.0, 19.3, 16.5, 19.5, 18.6, 20.0, 18.8, 19.1, 17.5, 18.5, 18.0};
+  std::vector<double> v{36, 25, 32, 38, 28, 35, 27, 20, 15, 29, 18, 22};
   std::sort(v.begin(), v.end());
   size_t count = 1;
   for (double i : v) std::cout << '\n' << count++ << ' ' << i;
